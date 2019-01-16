25f08432f2e5edb922171d4f1ec10cd59b8aaad7
drunken_j <mycoolade@gmail.com>
2019-01-17 01:51:08 +0900

    사용하지 않는 파일 제거 (i18n localize vue file) / pkg add

--- src/i18n.js
+++ src/i18n.js
@@ -3,18 +3,6 @@ import VueI18n from 'vue-i18n'
 
 Vue.use(VueI18n)
 
-function loadLocaleMessages () {
-    const locales = require.context('./locale', true, /[A-Za-z0-9-_,\s]+\.json$/i)
-    const messages = {}
-    locales.keys().forEach(key => {
-        const matched = key.match(/([A-Za-z0-9-_]+)\./i)
-        if (matched && matched.length > 1) {
-            const locale = matched[1]
-            messages[locale] = locales(key)
-        }
-    })
-    return messages
-}
 
 export default new VueI18n({
     locale: ( () => {
@@ -22,5 +10,4 @@ export default new VueI18n({
         return /ko/.test(userAgent) ? 'ko' : 'en';
     })(),
     fallbackLocale: 'en',
-    messages: loadLocaleMessages()
 })
